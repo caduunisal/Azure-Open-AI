0763c4c7dc83dda10c1c308cd354416910fc1b4f
caduunisal <kadu.unisal1@gmail.com>
2024-10-18 12:00:24 -0300

    Fixing OpenAI Authentication

--- open-ai.py
+++ open-ai.py
@@ -6,16 +6,19 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def ler_arquivo(arquivo):
     with open(arquivo, 'r', encoding='utf-8') as file:
         return file.read()
     
 def resumo(texto):
     response = openai.completions.create(
-        engine="text-davinci-003",
+    # response = openai.Completion.create(
+        # engine="text-davinci-003",
+        model="gpt-3.5-turbo",
         prompt=f"Resuma o seguinte texto {texto}",
         temperature=0.7,
-        max_tokens=2048,
+        max_tokens=200,
         n=1,
         stop=None
     )
@@ -24,6 +27,8 @@ def resumo(texto):
 arquivo = 'sistema_solar.txt'
 texto = ler_arquivo(arquivo)
 
+# print(texto)
 print(resumo(texto))
+
     
 
