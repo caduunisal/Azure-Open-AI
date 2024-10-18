22ca5c077f6e88820350c278aa2167517fb69393
caduunisal <kadu.unisal1@gmail.com>
2024-10-18 09:43:28 -0300

    Adding python functions to read text file

--- open-ai.py
+++ open-ai.py
@@ -1,3 +1,14 @@
 import openai
-from dotenv 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def ler_arquivo(arquivo):
+    with open(arquivo, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+
+def resumo(texto):
+    
 
